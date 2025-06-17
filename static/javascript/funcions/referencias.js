93c8556cf39621645441ebc5737ed99955a3a082
cursosisraelsp <cursos.israelsp@gmail.com>
2025-06-17 08:40:45 +0200

    Exercicios folla 1

--- static/javascript/funcions/referencias.js
+++ static/javascript/funcions/referencias.js
@@ -1,10 +1,22 @@
 let refParrafo1 = document.querySelector("#parrafo1");
+let refBoton = document.querySelector("#boton");
 let refDiv1 = document.querySelector(".div1");
 let refLista = document.querySelector(".lista");
-let refBoton = document.querySelector("#boton");
+let refTabla = document.querySelector("table"); 
+let refTablaTr = document.querySelector("tr");
+let refTablaTrTodos = document.querySelectorAll("tr");
+let refTablaTh = refTablaTrTodos[0].querySelectorAll("th");
+let refBody1 = document.querySelector("body");
+let refBody2 = document.body;
 export {
     refParrafo1,
     refDiv1,
     refLista,
-    refBoton
+    refBoton,
+    refTabla,
+    refTablaTr,
+    refTablaTrTodos,
+    refTablaTh,
+    refBody1,
+    refBody2
 }
--- static/javascript/index.js
+++ static/javascript/index.js
@@ -2,20 +2,40 @@ import {
     refParrafo1,
     refDiv1 ,
     refLista,
-    refBoton
+    refBoton,
+    refTabla,
+    refTablaTr,
+    refTablaTrTodos,
+    refTablaTh,
+    refBody1,
+    refBody2
 } from "./funcions/referencias.js";
-
+// PARRAFO
 console.log(refParrafo1);
 console.log(refParrafo1.innerHTML);
 console.log(refParrafo1.textContent);
 console.log(refDiv1);
 console.log(refDiv1.innerHTML);
 console.log(refDiv1.textContent);
+// LISTA
 console.log(refLista);
 console.log(refLista.children)
-console.log(refLista.children[0])
+console.log(refLista.children[0])// ATRAPAMOS O PRIMEIRO ELEMENTO DA LISTA
 console.log(refLista.children[0].textContent)
+// TABLA 
+console.log(refTabla);
+// o tr
+console.log(refTablaTr);
+// todos os tr
+console.log(refTablaTrTodos);
+// un th
+console.log(refTablaTh);
 
+// BODY
+console.log(refBody1);
+console.log(refBody2);
+
+/// EVENTOS
 refBoton.addEventListener("click", function(){
     console.log(refLista);
 })
