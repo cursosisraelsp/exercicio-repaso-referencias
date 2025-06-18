1890d1769787d685f6c63802e9ec637cdd2cd13a
cursosisraelsp <cursos.israelsp@gmail.com>
2025-06-18 14:32:41 +0200

    editar gardar e borrar

--- static/javascript/funcions/referencias.js
+++ static/javascript/funcions/referencias.js
@@ -1,5 +1,6 @@
 let refLista = document.querySelector(".lista");
 let refLista2 = document.querySelector(".lista2");
+let refListaImaxes = document.querySelectorAll("img")
 //let refImg = document.querySelector("img")
 // EXER 4
 // let ref1ParrafoDendeLista = refLista.children[0];// sitúo a referencia no primeiro dato da lista : Item 1
@@ -9,5 +10,6 @@ let ref1ParrafoDendeLista = refLista.children[2];// sitúo a referencia no prime
 export {
     ref1ParrafoDendeLista,
     refLista,
-    refLista2
+    refLista2,
+    refListaImaxes
 }
--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,7 +1,8 @@
 import { 
     ref1ParrafoDendeLista,
     refLista,
-    refLista2
+    refLista2,
+    refListaImaxes
  } from "./funcions/referencias.js";
 
 /**
@@ -37,6 +38,7 @@ Lembra de referenciar a lista (‘ul’)
 correctamente.
 Emprega o evento “click”.
  */
+
 refLista2.addEventListener("click",(event)=>{
     //console.log("event",event)// NO MOMENTO QUE CLIQUE TRAE MOITA INFORMACIÓN DO ELEMENTO CLICADO
     console.log("event.target",event.target)// ELEMENTO ONDE CLICAS, SACA A ETIQUETA CLICADA
@@ -49,7 +51,45 @@ refLista2.addEventListener("click",(event)=>{
         // TEÑO QUE SITUARME DENDE O ELEMENTO CLICADO QUE NESTE CASO É img
         // ACCEDO O PAI mediante parentElement
         // UTILIZO remove() PARA BORRAR
-        //event.target.parentElement.remove()
-        console.log("clico a imaxen")
+        event.target.parentElement.remove()
+        //console.log("clico a imaxen")
+    }
+    if(event.target.getAttribute("name") == "guardar"){
+
+        console.log("clico imaxen gardar")
+        event.target.parentElement.contentEditable = false
+    }
+    if(event.target.getAttribute("name") == "editar"){
+
+        console.log("clico imaxen editar")
+         event.target.parentElement.contentEditable = true
     }
 })
+
+
+/**
+ * Exercicio 8
+Ca lista anterior:
+➢ Cómo seleccionarías todas a imaxes para podelas utilizar.
+➢ Cómo lle engadirías un evento a estas imaxes se che dixeran que sólo podes
+    utilizar estas para xenerar a referencia ? [se non entendes este enunciado,avísame]
+➢ Cando saibas cómo engadirlle o envento na imaxen:
+    ○ Fai que saque por consola o texto asociado a ese ‘li’
+    ○ Fai que saque por consola a referencia da lista
+ */
+// IMAXEN DE MANEIRA INDIVIDUAL
+/*
+refListaImaxes[0].addEventListener("click",()=>{
+    console.log("dfasdfa")
+})*/
+
+// TODAS AS IMAXES
+/*
+for(let imaxe of refListaImaxes){
+    imaxe.addEventListener("click",()=>{
+        console.log("imaxen clicada")
+        imaxe.parentElement.remove()
+    })
+}
+*/
+console.log(refListaImaxes)
