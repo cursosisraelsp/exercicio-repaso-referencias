70b83e42790e71b9a39cb3bd9ec357e90df0e8aa
cursosisraelsp <cursos.israelsp@gmail.com>
2025-06-18 09:37:46 +0200

    Subimos uns exercicios

--- static/javascript/funcions/referencias.js
+++ static/javascript/funcions/referencias.js
@@ -1,26 +1,7 @@
-let refDiv1 = document.querySelector(".div1");
-let refParrafo1 = refDiv1.parentElement.children[0]
-let refParrafo2 = refDiv1.previousElementSibling
 let refLista = document.querySelector(".lista");
-let ref1ParrafoDendeLista = refLista.parentElement.children[0];
-let ref2ParrafoDendeLista = refLista.previousElementSibling.previousElementSibling;
-let refTablaDendeLista = refLista.nextElementSibling
-let refTabla = document.querySelector("table");
-let refTd1sTabla = refTabla.children[0].children[1].children
-let refDiv1DendeTabla = refTabla.previousElementSibling.previousElementSibling
-let refDiv2DendeTabla = refTabla.parentElement.children[1]
-let refTabla1DendeDiv = refDiv1.parentElement.children[3]
-let refTabla2DendeDiv = refDiv1.nextElementSibling.nextElementSibling
+let ref1ParrafoDendeLista = refLista.children[0];// sitúo a referencia no primeiro dato da lista : Item 1
+
 export {
-    refParrafo1,
-    refParrafo2,
-    refLista,
     ref1ParrafoDendeLista,
-    ref2ParrafoDendeLista,
-    refTablaDendeLista,
-    refTd1sTabla,
-    refDiv1DendeTabla,
-    refDiv2DendeTabla,
-    refTabla1DendeDiv,
-    refTabla2DendeDiv
+    refLista
 }
--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,78 +1,21 @@
-import {
-    refParrafo1,
-    refParrafo2,
-    refLista,
+import { 
     ref1ParrafoDendeLista,
-    ref2ParrafoDendeLista,
-    refTablaDendeLista,
-    refTd1sTabla,
-    refDiv1DendeTabla,
-    refDiv2DendeTabla,
-    refTabla1DendeDiv,
-    refTabla2DendeDiv
-} from "./funcions/referencias.js";
-// EXER1 - referencia o párrafo dende o div
-/**
- * A partir da referencia creada no ‘div’, qué ‘propiedade’ ou ‘función’ utilizarías para
-acceder a etiqueta ‘p’?. Mostra o contido por consola cando a localizes.
- */
-//console.log("refDiv1.parentElement.children[0] O párrafo é o seguinte:",refParrafo1)
-//console.log("refDiv1.previousElementSibling O párrafo é o seguinte:",refParrafo2)
+    refLista
+ } from "./funcions/referencias.js";
 
-// EXER2
 /**
- * A partir da referencia creada na lista a ‘ul’, qué ‘propiedade’ ou función utilizarías
-para acceder as diferentes etiquetas filla? . Mostra por consola ditos contidos.
+ * Exercicio 4
+Introduce na lista anterior un evento
+“click” cada vez que lle des no primeiro
+elemento da lista, saca por consola:
+➢ Toda a lista creada
+➢ O texto dese elemento
+
+Emprega o evento “click”.
  */
 
-//console.log(refLista.children)
-//console.log(refLista.children[0])
-//console.log(refLista.children[0].textContent)
+ref1ParrafoDendeLista.addEventListener("click",()=>{
+    console.log("sae un ...",ref1ParrafoDendeLista.textContent)
+    console.log("refLista ",refLista)
+})
 
-// EXER3
-/**
- * 	A partir da referencia creada na lista a ‘ul’,
- *  qué ‘propiedade’ ou función utilizarías para acceder a etiqueta ‘p’? . 
- *  Mostra por consola o contido.
- */
-
-//## Opcion 1
-//console.log(ref1ParrafoDendeLista.textContent,ref1ParrafoDendeLista)
-//## Opcion 2
-//console.log(ref2ParrafoDendeLista.textContent,ref2ParrafoDendeLista)
-
-// EXER4
-/**
- * A partir da referencia creada na lista a ‘ul’, qué ‘propiedade’ ou función utilizarías
-para acceder a etiqueta ‘table’? . Mostra por consola o contido.
-Unha vez, que accedas, cómo accederías a algunha das súas etiquetas ‘td’?
- */
-
-//console.log(refTablaDendeLista)// acceso a etiqueta table
-//console.log(refTd1sTabla)
-
-// EXER5
-/**
- * A partir da referencia creada na etiqueta tabla,
- * cómo accederías a etiqueta ‘div’?.
- * Mostra por consola o ser contido
- */
-//console.log(refDiv1DendeTabla)
-//console.log(refDiv2DendeTabla)
-
-// EXER6 -- o omito realizar , xa que previamente xa o realizamos
-/**
- * A partir da referencia creada na etiqueta tabla, 
- * cómo accederías as diferentes etiquetas ‘td’?. 
- * Mostra por consola o ser contido.
- */
-
-// EXER7
-/**
- * A partir da referencia creada no ‘div’, 
- * qué ‘propiedade’ ou ‘función’ utilizarías para acceder a etiqueta ‘table’?. 
- * Mostra o contido por consola cando a localizes.
- */
-
-console.log(refTabla1DendeDiv)
-console.log(refTabla2DendeDiv)
